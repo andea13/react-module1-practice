c814e3ccb042bf166c57a40cd17b393bbdaef3d7
Oksana Dziuba <dziuba131303@gmail.com>
2023-07-18 10:51:40 +0400

    init

---
+++ src/index.js
@@ -0,0 +1,70 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+// import ReactDOM from "react-dom/client";
+// import './index.css';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
+
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
+
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+// const elem1 = React.createElement("span", { children: "Hello" });
+// const elem2 = React.createElement("span", { children: "World" });
+
+// const element = React.createElement("div", {
+//   a: 5,
+//   b: 10,
+//   children: "Hello world",
+// });
+
+// const elem1 = <span>Hello</span>;
+// const elem2 = <span>World</span>;
+
+// const jsxElement = (
+//   <div>
+//     {elem1}
+//     {elem2}
+//   </div>
+// );
+// console.log(jsxElement);
+
+// const divEl = document.getElementById("root");
+// ReactDOM.createRoot(divEl).render(element);
+
+//>>>>>>>>Component Painting<<<<<<<<<<<<<<<<<
+{
+  /* <div>
+  <img src="" alt="" width="480" />
+  <h2></h2>
+  <p>
+    Автор: <a href=""></a>
+  </p>
+  <p>Ціна: кредитів</p>
+  <p>Доступність: закінчується чи є в наявності</p>
+  <button type="button">Додати до корзини</button>
+</div>; */
+}
+
+const painting = (
+  <div>
+    <img src="" alt="" width="480" />
+    <h2></h2>
+    <p>
+      Автор: <a href=""></a>
+    </p>
+    <p>Ціна: кредитів</p>
+    <p>Доступність: закінчується чи є в наявності</p>
+    <button type="button">Додати до корзини</button>
+  </div>
+);
